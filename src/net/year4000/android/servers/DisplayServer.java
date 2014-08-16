0035cdb91ae0926bf435a397f3439363339e540e
jsho32 <justin.shores32@gmail.com>
2014-08-16 11:21:38 -0700

    added formatting colors in description

--- src/net/year4000/android/servers/DisplayServer.java
+++ src/net/year4000/android/servers/DisplayServer.java
@@ -5,11 +5,15 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.Html;
 import android.widget.TextView;
 
 import com.google.common.base.Joiner;
 
 import net.year4000.android.R;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class DisplayServer extends Activity {
     private String chosen;
@@ -38,7 +42,7 @@ public class DisplayServer extends Activity {
                 }
                 else {
                     Server.Players players = server.getStatus().getPlayers();
-                    text.append(server.getStatus().getDescription() + "\n");
+                    text.append(Html.fromHtml(formatDescription(server.getStatus().getDescription())) + "\n");
                     text.append(String.format("Players (%d/%d) \n", players.getOnline(), players.getMax()));
 
                     if (server.isSample()) {
@@ -47,6 +51,42 @@ public class DisplayServer extends Activity {
                 }
             }
         });
+    }
+
+    public String formatDescription(String des) {
+        HashMap<String, String> colors = new HashMap<String, String>();
+        colors.put("§a", "</span><span style='color:#5f5;'>");
+        colors.put("§b", "</span><span style='color:#5ff;'>");
+        colors.put("§c", "</span><span style='color:#f55;'>");
+        colors.put("§d", "</span><span style='color:#f5f;'>");
+        colors.put("§e", "</span><span style='color:#ff5;'>");
+        colors.put("§f", "</span><span style='color:#fff;'>");
+        colors.put("§0", "</span><span style='color:#000;'>");
+        colors.put("§1", "</span><span style='color:#00a;'>");
+        colors.put("§2", "</span><span style='color:#0a0;'>");
+        colors.put("§3", "</span><span style='color:#0aa;'>");
+        colors.put("§4", "</span><span style='color:#a00;'>");
+        colors.put("§5", "</span><span style='color:#a0a;'>");
+        colors.put("§6", "</span><span style='color:#fa0;'>");
+        colors.put("§7", "</span><span style='color:#aaa;'>");
+        colors.put("§8", "</span><span style='color:#555;'>");
+        colors.put("§9", "</span><span style='color:#55f;'>");
+        colors.put("§k", "</span><span style='color:#fff;'>");
+        colors.put("§o", "</span><span style='color:#fff;'>");
+        colors.put("§l", "</span><span style='color:#fff;'>");
+        colors.put("§m", "</span><span style='color:#fff;'>");
+        colors.put("§r", "</span><span style='color:#fff;'>");
+        colors.put("WAITING", "</span><span style='color:#ff5;'>WAITING</span>");
+        colors.put("STARTING", "</span><span style='color:#0a0;'>STARTING</span>");
+        colors.put("PLAYING", "</span><span style='color:#5f5;'>PLAYING</span>");
+        colors.put("ENDING", "</span><span style='color:#f55;'>ENDING</span>");
+        for(Map.Entry<String, String> entry : colors.entrySet()) {
+            String key = entry.getKey();
+            String value = entry.getValue();
+            des = des.replace(key, value);
+        }
+
+        return des;
     }
 
     private class PostFetcher extends AsyncTask<Void, Void, String> {
