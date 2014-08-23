8fd8f41f3b91852c7aa3862336060cb5d1c98084
jsho32 <justin.shores32@gmail.com>
2014-08-22 19:21:38 -0700

    resolve screen rotation crash bug

--- android-app/src/main/java/net/year4000/android_app/servers/ServersActivity.java
+++ android-app/src/main/java/net/year4000/android_app/servers/ServersActivity.java
@@ -2,13 +2,18 @@ package net.year4000.android_app.servers;
 
 import android.annotation.TargetApi;
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.app.ProgressDialog;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.widget.SwipeRefreshLayout;
+import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AbsListView;
 import android.widget.ExpandableListView;
 import net.year4000.android_app.R;
@@ -24,7 +29,7 @@ public class ServersActivity extends Activity {
     private ExpandableListView expandList;
     private static final String TAG = "ServersActivity";
     private SwipeRefreshLayout swipeView;
-    private PostFetcher fetcher;
+    private FetcherFragment fetcher;
 
     /** Called when the activity is first created. */
     @TargetApi(Build.VERSION_CODES.CUPCAKE)
@@ -32,24 +37,35 @@ public class ServersActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.servers_activity);
-        fetcher = new PostFetcher(ServersActivity.this);
-        fetcher.execute();
+        fetcher = new FetcherFragment();
+        setFragment(fetcher);
         swipeView = (SwipeRefreshLayout)findViewById(R.id.swipe);
         swipeView.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
                 swipeView.setRefreshing(true);
-                ( new Handler()).postDelayed(new Runnable() {
+                (new Handler()).postDelayed(new Runnable() {
                     @TargetApi(Build.VERSION_CODES.CUPCAKE)
                     @Override
                     public void run() {
                         swipeView.setRefreshing(false);
-                        fetcher = new PostFetcher(ServersActivity.this);
-                        fetcher.execute();
+                        fetcher = new FetcherFragment();
+                        setFragment(fetcher);
                     }
                 }, 3000);
             }
         });
+    }
+
+    /** Set up and start the fetcher fragment */
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    public void setFragment(Fragment frag)
+    {
+        FragmentManager fm = getFragmentManager();
+        if (fm.findFragmentByTag("FETCHER_FRAG") == null) {
+            fm.beginTransaction().add(R.id.swipeContainer, frag).commit();
+        }
+
     }
 
     @Override
@@ -59,7 +75,8 @@ public class ServersActivity extends Activity {
 
         return true;
     }
-    /**runs on the thread*/
+
+    /** runs on the thread */
     private void serverList() {
         runOnUiThread(new Runnable() {
             @Override
@@ -68,23 +85,23 @@ public class ServersActivity extends Activity {
                 expAdapter = new ExpandListAdapter(ServersActivity.this, expListItems);
                 expandList.setAdapter(expAdapter);
                 setListScrollListener(expandList);
-
             }
         });
     }
-    /**this only allows swipeRefresh if first child of list is visible*/
+
+    /** this only allows swipeRefresh if first child of list is visible */
     public void setListScrollListener(final ExpandableListView list) {
         list.setOnScrollListener(new AbsListView.OnScrollListener() {
             @Override
-            public void onScrollStateChanged(AbsListView absListView, int i) { }
+            public void onScrollStateChanged(AbsListView absListView, int i) {
+            }
 
             @Override
             public void onScroll(AbsListView absListView, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
 
                 if (firstVisibleItem == 0 && visibleItemCount > 0 && list.getChildAt(0).getTop() >= 0) {
                     swipeView.setEnabled(true);
-                }
-                else {
+                } else {
                     swipeView.setEnabled(false);
                 }
 
@@ -103,34 +120,66 @@ public class ServersActivity extends Activity {
         return list;
     }
 
-    private class PostFetcher extends AsyncTask<Void, Void, String> {
-        private static final String TAG = "PostFetcher";
-        private ProgressDialog dialog;
-        private Activity context;
+    /** fragment the async to prevent crash on app disruption */
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    public class FetcherFragment extends Fragment  {
 
-        public PostFetcher(Activity mainActivity) {
-            this.context = mainActivity;
-            dialog = new ProgressDialog(context);
+        public static final String FETCHER_FRAG_TAG = "FETCHER_FRAG";
+        private ProgressDialog progressDialog;
+        private boolean isTaskRunning = false;
+
+        public FetcherFragment() {}
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setRetainInstance(true);
+            new PostFetcher().execute();
         }
 
         @Override
-        protected void onPreExecute() {
-            dialog.setMessage("Loading Server Info...");
-            dialog.show();
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            return inflater.inflate(R.layout.servers_activity, container, false);
+        }
+
+        /** async to communicate with API */
+        @TargetApi(Build.VERSION_CODES.CUPCAKE)
+        private class PostFetcher extends AsyncTask<Void, Void, String> {
+            private static final String TAG = "PostFetcher";
+
+            public PostFetcher() {}
+
+            @Override
+            protected void onPreExecute() {
+                isTaskRunning = true;
+                progressDialog = ProgressDialog.show(getActivity(), "Loading Server Info...", "Please wait! This will only take a moment.");
+            }
+
+            @Override
+            protected String doInBackground(Void... params) {
+                APIManager.get().pullAPI();
+                expListItems = setStandardGroups();
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(String result) {
+                if (progressDialog != null) {
+                    progressDialog.dismiss();
+                }
+                serverList();
+                isTaskRunning = false;
+            }
+
         }
 
         @Override
-        protected String doInBackground(Void... params) {
-            APIManager.get().pullAPI();
-            expListItems = setStandardGroups();
-            return null;
+        public void onDetach() {
+            // prevent Activity has leaked window com.android.internal.policy... exception
+            if (progressDialog != null && progressDialog.isShowing()) {
+                progressDialog.dismiss();
+            }
+            super.onDetach();
         }
-
-        @Override
-        protected void onPostExecute(String result) {
-            dialog.dismiss();
-            serverList();
-        }
-
     }
 }
