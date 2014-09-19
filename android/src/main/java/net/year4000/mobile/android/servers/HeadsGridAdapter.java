cdec3004594652d070b9f74d8efd026b61bf40d6
jsho32 <justin.shores32@gmail.com>
2014-09-18 17:14:33 -0700

    Add GridView Adapter.

---
+++ android/src/main/java/net/year4000/mobile/android/servers/HeadsGridAdapter.java
@@ -0,0 +1,53 @@
+package net.year4000.mobile.android.servers;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+public class HeadsGridAdapter extends BaseAdapter {
+
+    Context context;
+    Bitmap[] data = null;
+
+    public HeadsGridAdapter(Context context, Bitmap[] data) {
+        this.context = context;
+        this.data = data;
+    }
+
+    @Override
+    public int getCount() {
+        return data.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return data[position];
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View view, ViewGroup parent) {
+        ImageView headView;
+        if (view == null) {
+            Log.e("LOADING_IMAGE_VIEW", "HERE");
+            headView = new ImageView(context);
+            //headView.setLayoutParams(new GridView.LayoutParams(40, 40));
+            headView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            headView.setPadding(5, 5, 5, 5);
+            headView.setImageBitmap(data[position]);
+        } else {
+            headView = (ImageView) view;
+        }
+
+        return headView;
+    }
+}
