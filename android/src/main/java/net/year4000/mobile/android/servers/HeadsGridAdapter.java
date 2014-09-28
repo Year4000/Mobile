89f68333f7aa1c6aa1625dbabfa82d367dba5992
jsho32 <justin.shores32@gmail.com>
2014-09-28 14:13:18 -0700

    Show player name on grid cell click.

--- android/src/main/java/net/year4000/mobile/android/servers/HeadsGridAdapter.java
+++ android/src/main/java/net/year4000/mobile/android/servers/HeadsGridAdapter.java
@@ -2,19 +2,29 @@ package net.year4000.mobile.android.servers;
 
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.TextView;
 
 public class HeadsGridAdapter extends BaseAdapter {
 
-    Context context;
-    Bitmap[] data = null;
+    private Context context;
+    private Bitmap[] data = null;
+    private String[] nameArray;
+    private TextView playerInfo;
+    private LinearLayout layoutOfPopup;
+    private PopupWindow playerInfoWindow;
 
-    public HeadsGridAdapter(Context context, Bitmap[] data) {
+    public HeadsGridAdapter(Context context, Bitmap[] data, String[] nameArray) {
         this.context = context;
         this.data = data;
+        this.nameArray = nameArray;
     }
 
     @Override
@@ -33,17 +43,64 @@ public class HeadsGridAdapter extends BaseAdapter {
     }
 
     @Override
-    public View getView(int position, View view, ViewGroup parent) {
-        ImageView headView;
+    public View getView(final int position, final View view, final ViewGroup parent) {
+        final ImageView headView;
         if (view == null) {
+            playerInfo = new TextView(context);
             headView = new ImageView(context);
-            //headView.setScaleType(ImageView.ScaleType.CENTER_CROP);
             headView.setPadding(5, 10, 5, 5);
             headView.setImageBitmap(data[position]);
+            headView.setClickable(true);
+
+
+            headView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (v.getId() == headView.getId()) {
+                        popupInit(headView);
+                        playerInfo.setText(nameArray[position]);
+                        for (int i = 0; i < parent.getChildCount(); i++) {
+                            parent.getChildAt(i).setBackgroundColor(Color.TRANSPARENT);
+                        }
+
+                        headView.setBackgroundColor(Color.parseColor("#1e6dc8"));
+                        playerInfoWindow.dismiss();
+                        playerInfoWindow.showAsDropDown(headView, 0, 0);
+                    }
+                    else {
+                        playerInfoWindow.dismiss();
+                    }
+                }
+            });
+
         } else {
             headView = (ImageView) view;
         }
 
         return headView;
+    }
+
+    public void popupInit(final View headview) {
+        if (layoutOfPopup != null) {
+            playerInfoWindow.dismiss();
+            layoutOfPopup.removeAllViews();
+        }
+
+        layoutOfPopup = new LinearLayout(context);
+        playerInfo.setPadding(5, 5, 5, 5);
+        playerInfo.setTextSize(20);
+        playerInfo.setClickable(true);
+        playerInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                headview.setBackgroundColor(Color.TRANSPARENT);
+                playerInfoWindow.dismiss();
+            }
+        });
+        layoutOfPopup.setBackgroundColor(Color.parseColor("#aa000000"));
+        layoutOfPopup.addView(playerInfo);
+        playerInfoWindow = new PopupWindow(layoutOfPopup, LayoutParams.WRAP_CONTENT,
+                LayoutParams.WRAP_CONTENT);
+        playerInfoWindow.setContentView(layoutOfPopup);
     }
 }
