60362a890a0bb78d776b8e6fc3f8269eddc583fa
jsho32 <justin.shores32@gmail.com>
2014-10-01 18:00:21 -0700

    Clean and take logic out of onCreate().

--- android/src/main/java/net/year4000/mobile/android/Year4000.java
+++ android/src/main/java/net/year4000/mobile/android/Year4000.java
@@ -2,31 +2,34 @@ package net.year4000.mobile.android;
 
 import android.app.Application;
 import android.content.Context;
-import android.graphics.BitmapFactory;
-import android.util.Log;
 
 import java.io.File;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
-public class Year4000 extends Application {
+import lombok.Getter;
 
+@Getter
+public class Year4000 extends Application {
     // max age of head file before re-downloading (change params as see fit).
     private long maxAge = TimeUnit.MILLISECONDS.convert(7, TimeUnit.DAYS);
+    private File headsDir;
 
     @Override
     public void onCreate() {
         super.onCreate();
+        deleteOldHeads();
+    }
+
+    private void deleteOldHeads() {
         Date currentDate = new Date();
-        File headsDir = this.getDir("headsDir", Context.MODE_PRIVATE);
+        headsDir = this.getDir("headsDir", Context.MODE_PRIVATE);
         File[] directoryListing = headsDir.listFiles();
         if (directoryListing != null) {
             for (File head : directoryListing) {
-                Log.e("CHECKING_HEAD", head.toString());
                 Date lastModifiedDate = new Date(head.lastModified());
                 long headsAge = currentDate.getTime() - lastModifiedDate.getTime();
                 if (headsAge >= maxAge) {
-                    Log.e("DELETING_HEAD", head.toString());
                     head.delete();
                 }
             }
