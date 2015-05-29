d23defc7d788f21dc8d4bb1af7cc8946e70692a0
jsho32 <justin.shores32@gmail.com>
2015-05-28 21:04:05 -0700

    Add fragment for shop.

---
+++ android/src/main/java/net/year4000/mobile/android/shop/ShopFragment.java
@@ -0,0 +1,25 @@
+package net.year4000.mobile.android.shop;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import net.year4000.mobile.R;
+
+public class ShopFragment extends Fragment {
+
+    /** When fragment is created */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    /** When fragment view is created */
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        final View layout = inflater.inflate(R.layout.fragment_shop, container, false);
+
+        return layout;
+    }
+}
