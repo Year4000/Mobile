d153ac20c5a55669c49fff69d11f82f05fe5e8b2
jsho32 <justin.shores32@gmail.com>
2015-05-28 19:57:46 -0700

    Implement first stage of MainActivity.

--- android/src/main/java/net/year4000/mobile/android/MainActivity.java
+++ android/src/main/java/net/year4000/mobile/android/MainActivity.java
@@ -1,19 +1,27 @@
 package net.year4000.mobile.android;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import android.view.View;
 import android.widget.TextView;
+import lombok.Getter;
 import net.year4000.mobile.R;
+import net.year4000.mobile.android.news.NewsFragment;
 import net.year4000.mobile.android.servers.ServersActivity;
 
-
-public class MainActivity extends Activity {
+@Getter
+public class MainActivity extends FragmentActivity {
+    private TextView servers;
+    private TextView settings;
+    private Fragment newsFragment;
+    private Fragment serversFragment;
+    private Fragment currentFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +32,13 @@ public class MainActivity extends Activity {
         } else {
             setContentView(R.layout.activity_main_land);
         }
+
+        newsFragment = new NewsFragment();
+        //serversFragment = new ServersFragment();
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.content, newsFragment)
+                .commitAllowingStateLoss();
+        currentFragment = newsFragment;
 
         initializeButtons();
 
@@ -46,7 +61,7 @@ public class MainActivity extends Activity {
     }
 
     private void initializeButtons() {
-        TextView servers = (TextView) findViewById(R.id.servers_button);
+        servers = (TextView) findViewById(R.id.servers_button);
         servers.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -55,7 +70,7 @@ public class MainActivity extends Activity {
             }
         });
 
-        TextView settings = (TextView) findViewById(R.id.settings_button);
+        settings = (TextView) findViewById(R.id.settings_button);
         settings.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -64,6 +79,13 @@ public class MainActivity extends Activity {
             }
         });
 
+    }
+
+    /** Switch UI to the given fragment */
+    void switchToFragment(Fragment newFrag) {
+        getSupportFragmentManager().beginTransaction().replace(R.id.content, newFrag)
+                .commitAllowingStateLoss();
+        currentFragment = newFrag;
     }
 }
 
